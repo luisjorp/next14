77f39088d71e96ef81092546a428a027d50f7049
LuisJo <rp.luisjose@gmail.com>
2024-06-20 13:50:16 -0600

    ADD: fonts from google

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import styles from '@/app/ui/home.module.css';
-
+import { lusitana } from '@/app/ui/fonts';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 
@@ -15,7 +15,10 @@ export default function Page() {
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Welcome to Acme 😀</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
+            <a
+              href="https://nextjs.org/learn/"
+              className={`${lusitana.className} text-blue-500`}
+            >
               Next.js Learn Course
             </a>
             , brought to you by Vercel.
---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Lusitana, Montserrat } from 'next/font/google';
+
+export const inter = Montserrat({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+});
+
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
